e2e7c4886abc9776e924b07db53f20a2d1ffc147
Wang <gavin19950511@gmail.com>
2017-05-29 23:07:39 +0800

    Update Backup.py

--- Backup.py
+++ Backup.py
@@ -1,4 +1,4 @@
-import os,sys,logging,platform
+import os,sys,logging,platform,time
 
 
 
@@ -6,60 +6,49 @@ import os,sys,logging,platform
 def CopyFile(SourceDir,FileName,SaveDir):
 	logging.basicConfig(level=logging.CRITICAL)#INFO,WORRING,ERROR,CRITICAL
 
-	#if(getattr(sys, "getwindowsversion", None)):
-	#    logging.info("Windows version   : %s", sys.getwindowsversion())
-	 
-	logging.info("Your OS is : %s", platform.system())
-	OS = platform.system()
 
-	if OS == "Linux":
-		try:
-			#cheak dir exists
-			if not os.path.exists(SaveDir):
-	    			os.makedirs(SaveDir)
-			#copy file
-			import shutil
+	try:
+		
+		#copy file
+		import shutil
+		
+		filename1 = os.path.join(SourceDir, FileName)
+		filename2 = os.path.join(SaveDir, FileName)
 
-			filename1 = os.path.join(SourceDir, FileName)
-			filename2 = os.path.join(SaveDir, FileName)
+		print filename1, "=>", filename2
 
-			print filename1, "=>", filename2
-	
 
-			shutil.copy2(filename1,filename2)
+		shutil.copy2(filename1,filename2)
 
-			if os.path.isfile (filename2): print "Success"
-		except:
-			logging.warning("Unexpected error:"+str(sys.exc_info()))
+		if os.path.isfile (filename2): print "Success"
+		
+		Text = filename1+"=>"+filename2
+		w = open(os.path.join(SaveDir, "CopyNote.txt"),'a')
+		w.writelines(Text+"\n")
+		w.close()
+	except:
+		logging.warning("Unexpected error:"+str(sys.exc_info()))
 			
 
-	if OS == "Windows":
-		try:
-			#cheak dir exists
-			if not os.path.exists(SaveDir):
-	    			os.makedirs(SaveDir)
-			#copy file
-			import shutil
 
-			filename1 = os.path.join(SourceDir, FileName)
-			filename2 = os.path.join(SaveDir, FileName)
-
-			print filename1, "=>", filename2
-	
-
-			shutil.copy2(filename1,filename2)
-
-			if os.path.isfile (filename2): print "Success"
-		except:
-			logging.warning("Unexpected error:"+str(sys.exc_info()))
 
 
 
 ## main thread
-
+print "e.g. txt"
 FileType = raw_input('File Type you want to back-up: ')#py
+print "e.g. C:\Users\Theta\Desktop"
 StartPoint = raw_input('Which is the file that you want to start searching: ')#/var/..../
+print "e.g. C:\Users\Theta\Desktop\NEW"
 BackupPoint = raw_input('where that you want to save copy: ')#/var/..../
+
+#cheak dir exists
+if not os.path.exists(BackupPoint):
+    os.makedirs(BackupPoint)
+#create note to record copy process
+w = open(os.path.join(BackupPoint, "CopyNote.txt"),'w')
+w.close()
+
 
 #search all subfile
 for dirPath, dirNames, fileNames in os.walk(StartPoint or "./"):
@@ -68,11 +57,12 @@ for dirPath, dirNames, fileNames in os.walk(StartPoint or "./"):
         #print os.path.join(dirPath, f)
 	if f[f.find(".")+1:]==FileType:
 		print os.path.join(dirPath, f)
-		CopyFile(dirPath,f,BackupPoint or "./")
+		CopyFile(dirPath,f,BackupPoint)
 	pass
 
 print("close program after 10s")
 time.sleep(5)
 print("close program after 5s")
 time.sleep(5)
+
 
