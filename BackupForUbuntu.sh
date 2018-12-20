fa5f004cd9b3ac63e62f546b6b240f5c3c5c65c0
Wang <gavin19950511@gmail.com>
2018-12-20 16:45:55 +0800

    Update BackupForUbuntu.sh

--- BackupForUbuntu.sh
+++ BackupForUbuntu.sh
@@ -1,35 +1,38 @@
 #backup and sftp to another pc.
 
-#cd /home  #base dir for put file
 
+
+BASE=/home/*****/Desktop/backup/Backupfolder
+#cd /home  #base dir for put file
 ID=20$(date +"%y_%m_%dv%I_%M_%S")
 
+
+#init folder
+echo "passwords" | sudo rm -rf ${BASE}
+mkdir ${BASE}
 # back up Mysql
-mysqldump -A > /home/administrator/BackupCommand/${ID}all_databases.sql -u root -pytwu57874 -h localhost
+mysqldump -A > ${BASE}/${ID}all_databases.sql -u root -proot -h localhost
 # restore Mysql
 #mysql <all_databases.sql -u root -pWulab35415
 
 
 # backup Web File
 # '--exclude' could skip dir you don't want to back up
-echo "Wulab35415" | sudo -S tar cvpzf /home/administrator/BackupCommand/${ID}backup.tgz /var/www/
+#echo "Wulab35415" | sudo -S tar cvpzf /home/administrator/BackupCommand/${ID}backup.tgz /var/www/
 
 
-HOST=140.115.126.216
-USER=gavin	
-PASSWD=gavin
-TargetDir=/home/BackupOfWuLAB/126_18 #Target dir for put file
+HOST=***.***.***.***
+USER=*****	
+PASSWD=*****	
+TargetDir=/home/BackupOfWuLAB/126_20 #Target dir for put file
 echo $ID
 
 lftp<<END_SCRIPT
-
 open sftp://$HOST
 set sftp:auto-confirm yes
 user $USER $PASSWD
 cd $TargetDir
-put /home/administrator/BackupCommand/${ID}all_databases.sql
-put /home/administrator/BackupCommand/${ID}backup.tgz
-
+put ${BASE}/${ID}all_databases.sql
 END_SCRIPT
 #end
 
